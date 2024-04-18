9cf71743f86656182990caf6937e0819a5f9e18d
Sayyadsulthan <asulthan088@gmail.com>
2024-04-18 14:06:01 +0530

    resolved calender show the status correct status and the adding members to group integration done

--- src/components/Calender/DateCalender.jsx
+++ src/components/Calender/DateCalender.jsx
@@ -1,134 +1,113 @@
-import React, { useState, useEffect, useMemo } from "react";
-import dayjs from "dayjs";
-import { Badge } from "@mui/material";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { PickersDay } from "@mui/x-date-pickers/PickersDay";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
-import VerifiedIcon from "@mui/icons-material/Verified";
-import DoDisturbIcon from "@mui/icons-material/DoDisturb";
-import { color } from "@mui/system";
+import React, { useState, useEffect, useMemo } from 'react';
+import dayjs from 'dayjs';
+import { Badge } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import DoDisturbIcon from '@mui/icons-material/DoDisturb';
+import { color } from '@mui/system';
 
-const initialValue = dayjs(new Date().toISOString().split("T")[0]);
+const initialValue = dayjs(new Date().toISOString().split('T')[0]);
 
 function ServerDay(props) {
-  const {
-    highlightedDays = [],
-    day,
-    outsideCurrentMonth,
-    startDate,
-    ...other
-  } = props;
-  const specificDate = day;
-  const specificDateString = specificDate.toISOString().split("T")[0];
+    const { highlightedDays = [], day, outsideCurrentMonth, startDate, ...other } = props;
+    const specificDate = day;
+    const specificDateString = specificDate.toISOString().split('T')[0];
 
-  const isSelected =
-    !outsideCurrentMonth &&
-    highlightedDays.some((date) => date.startsWith(specificDateString));
+    const isSelected =
+        !outsideCurrentMonth && highlightedDays.some((date) => date.startsWith(specificDateString));
 
-  const isBeforeCurrentDate = specificDate.isBefore(dayjs(), "day");
-  const isAfterStartDate =
-    specificDate.isSame(startDate, "day") ||
-    specificDate.isAfter(startDate, "day");
+    const isBeforeCurrentDate = specificDate.isBefore(dayjs(), 'day');
+    const isAfterStartDate =
+        specificDate.isSame(startDate, 'day') || specificDate.isAfter(startDate, 'day');
 
-  const isNotCompleted =
-    isAfterStartDate &&
-    isBeforeCurrentDate &&
-    !isSelected &&
-    !highlightedDays.includes(specificDateString);
+    const isNotCompleted =
+        isAfterStartDate &&
+        isBeforeCurrentDate &&
+        !isSelected &&
+        !highlightedDays.includes(specificDateString);
 
-  const badgeContent = isSelected ? (
-    <VerifiedIcon />
-  ) : isNotCompleted ? (
-    <DoDisturbIcon />
-  ) : null;
+    const badgeContent = isSelected ? <VerifiedIcon /> : isNotCompleted ? <DoDisturbIcon /> : null;
 
-  return (
-    <Badge
-      key={day.toISOString()}
-      overlap="circular"
-      badgeContent={badgeContent}
-    >
-      <PickersDay
-        {...other}
-        outsideCurrentMonth={outsideCurrentMonth}
-        day={day}
-      />
-    </Badge>
-  );
+    return (
+        <Badge key={day.toISOString()} overlap="circular" badgeContent={badgeContent}>
+            <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
+        </Badge>
+    );
 }
 
 export default function DateCalendarServerRequest({ progress, challenge }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [startDate, setStartDate] = useState(dayjs(challenge.startDate));
-  const [endDate, setEndDate] = useState(dayjs(challenge.endDate));
+    const [isLoading, setIsLoading] = useState(false);
+    const [startDate, setStartDate] = useState(dayjs(challenge.startDate));
+    const [endDate, setEndDate] = useState(dayjs(challenge.endDate));
 
-  const currentDate = new Date();
-  const currentDateLocalISOString = new Date(
-    currentDate.getTime() + currentDate.getTimezoneOffset() * 60000
-  ).toISOString();
+    const currentDate = new Date();
+    const currentDateLocalISOString = new Date(
+        currentDate.getTime() + currentDate.getTimezoneOffset() * 60000
+    ).toISOString();
 
-  let dataFromServer = progress.data.filter((val) => val.completed === true);
+    let dataFromServer = progress.data.filter((val) => val.completed === true);
 
-  const highlightedDays = useMemo(() => {
-    const filteredData = dataFromServer
-      .filter(
-        (item) =>
-          dayjs(item.date).isSame(startDate, "day") ||
-          dayjs(item.date).isAfter(startDate, "day")
-      )
-      .filter(
-        (item) =>
-          dayjs(item.date).isSame(endDate, "day") ||
-          dayjs(item.date).isBefore(endDate, "day")
-      )
-      .filter((item) =>
-        dayjs(item.date).isBefore(currentDateLocalISOString, "day")
-      )
-      .map((item) => dayjs(item.date).toISOString());
-    console.log("memo");
-    return filteredData;
-  }, [dataFromServer, startDate, endDate, challenge, progress]);
+    const highlightedDays = useMemo(() => {
+        const filteredData = dataFromServer
+            .filter(
+                (item) =>
+                    dayjs(item.date).isSame(startDate, 'day') ||
+                    dayjs(item.date).isAfter(startDate, 'day')
+            )
+            .filter(
+                (item) =>
+                    dayjs(item.date).isSame(endDate, 'day') ||
+                    dayjs(item.date).isBefore(endDate, 'day')
+            )
+            .filter((item) => dayjs(item.date).isBefore(currentDateLocalISOString, 'day'))
+            .map((item) => dayjs(item.date).toISOString());
+        console.log('memo', dataFromServer);
+        return filteredData;
+    }, [dataFromServer, startDate, endDate, challenge, progress]);
 
-  const handleMonthChange = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  };
+    const handleMonthChange = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    };
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        sx={{
-          width: 300,
-          color: "success.main",
-        }}
-        defaultValue={initialValue}
-        loading={isLoading}
-        onMonthChange={handleMonthChange}
-        renderLoading={() => <DayCalendarSkeleton />}
-        slots={{
-          day: (props) => (
-            <ServerDay
-              {...props}
-              startDate={startDate}
-              enddate={endDate} /* endDate={endDate}*/
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar
+                sx={{
+                    width: 300,
+                    color: 'success.main',
+                }}
+                defaultValue={initialValue}
+                loading={isLoading}
+                onMonthChange={handleMonthChange}
+                renderLoading={() => <DayCalendarSkeleton />}
+                slots={{
+                    day: (props) => (
+                        <ServerDay
+                            {...props}
+                            // startDate={startDate}
+                            startdate={startDate}
+                            enddate={endDate} /* endDate={endDate}*/
+                        />
+                    ),
+                }}
+                slotProps={{
+                    day: {
+                        highlightedDays,
+                    },
+                }}
             />
-          ),
-        }}
-        slotProps={{
-          day: {
-            highlightedDays,
-          },
-        }}
-      />
-    </LocalizationProvider>
-  );
+        </LocalizationProvider>
+    );
 }
 
 const style = {
-  color: "white",
-  background: "black",
+    color: 'white',
+    background: 'black',
 };
