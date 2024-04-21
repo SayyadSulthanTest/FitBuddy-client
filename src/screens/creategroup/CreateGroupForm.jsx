7e1023c4bf9820f5e7ab7865db9ca2125a468d6a
Sayyadsulthan <asulthan088@gmail.com>
2024-04-21 14:12:20 +0530

    Resolved the routing after create group

--- src/screens/creategroup/CreateGroupForm.jsx
+++ src/screens/creategroup/CreateGroupForm.jsx
@@ -1,123 +1,126 @@
-import React, { useState } from "react";
-import { TextField, Box } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import UploadImage from "../../components/UploadImage.jsx";
+import React, { useState } from 'react';
+import { TextField, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import UploadImage from '../../components/UploadImage.jsx';
 const CreateGroupForm = () => {
-  const navigate = useNavigate();
-  const [groupIcon, setGroupIcon] = useState("");
-  const [groupName, setGroupName] = useState("");
-  const [groupDescription, setGroupDescription] = useState("");
+    const navigate = useNavigate();
+    const [groupIcon, setGroupIcon] = useState('');
+    const [groupName, setGroupName] = useState('');
+    const [groupDescription, setGroupDescription] = useState('');
 
-  const handleReset = () => {
-    setGroupName("");
-    setGroupDescription("");
-  };
+    const handleReset = () => {
+        setGroupName('');
+        setGroupDescription('');
+    };
 
-  const handleCreateGroupSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const token = localStorage.getItem("token");
-      // api request and then set the other values to initial values
-      const API_URL = import.meta.env.VITE_API_URL;
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      };
-      const body = {
-        name: groupName,
-        description: groupDescription,
-        icon: groupIcon,
-      };
+    const handleCreateGroupSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem('token');
+            // api request and then set the other values to initial values
+            const API_URL = import.meta.env.VITE_API_URL;
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            };
+            const body = {
+                name: groupName,
+                description: groupDescription,
+                icon: groupIcon,
+            };
 
-      // if (previewImage) body.icon = previewImage;
+            // if (previewImage) body.icon = previewImage;
+            if (data.success) {
+                const { data } = await axios.post(`${API_URL}/groups`, body, config);
+                console.log('data: ', data);
 
-      const { data } = await axios.post(`${API_URL}/groups`, body, config);
-      console.log("data: ", data);
-      if (data.success) {
-        const groupId = data.data._id;
-        toast.success("Group created!");
-        navigate(`/privategroup/${groupId}`);
-      }
-      // const groupId = data
-      // handleReset();
-      // navigate('/createchallenge');
-    } catch (err) {
-      toast.error("Failed to create group");
-      console.log("Error in create Group: ", err);
-    }
-  };
+                const groupId = data.data._id;
+                toast.success('Group created!');
+                return navigate(`/privategroup/${groupId}`);
+            }
 
-  return (
-    <div
-      className="creategroup-container"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-      }}
-    >
-      <div
-        className="creategroup-form"
-        style={{
-          width: "50%",
-          margin: "auto",
-          minWidth: "350px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+            toast.error('Failed to create!!');
+            // const groupId = data
+            // handleReset();
+            // navigate('/createchallenge');
+        } catch (err) {
+            toast.error('Failed to create group');
+            console.log('Error in create Group: ', err);
+        }
+    };
+
+    return (
         <div
-          style={{
-            width: "90%",
-            backgroundColor: "#CFDBF8",
-            color: "black",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+            className="creategroup-container"
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100vw',
+            }}
         >
-          <small>Add Group Icon here</small>
-          <UploadImage picUrl={groupIcon} changePicUrl={setGroupIcon} />
-        </div>
+            <div
+                className="creategroup-form"
+                style={{
+                    width: '50%',
+                    margin: 'auto',
+                    minWidth: '350px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <div
+                    style={{
+                        width: '90%',
+                        backgroundColor: '#CFDBF8',
+                        color: 'black',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <small>Add Group Icon here</small>
+                    <UploadImage picUrl={groupIcon} changePicUrl={setGroupIcon} />
+                </div>
 
-        <form
-          onSubmit={handleCreateGroupSubmit}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "0.5rem",
-            width: "90%",
-            backgroundColor: "#CFDBF8",
-            padding: "0.6rem",
-            marginBottom: "2.4rem",
-          }}
-        >
-          <TextField
-            label="Group Name:"
-            type="text"
-            value={groupName}
-            onChange={(e) => setGroupName(e.target.value)}
-            placeholder="Group name here..."
-            required
-            sx={{ mb: 1, width: "90%" }}
-          />
-          <TextField
-            label="Group Description:"
-            type="text"
-            value={groupDescription}
-            onChange={(e) => setGroupDescription(e.target.value)}
-            placeholder="Group Description here..."
-            required
-            sx={{ mb: 1, width: "90%" }}
-          />
-          {/* <TextField
+                <form
+                    onSubmit={handleCreateGroupSubmit}
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '0.5rem',
+                        width: '90%',
+                        backgroundColor: '#CFDBF8',
+                        padding: '0.6rem',
+                        marginBottom: '2.4rem',
+                    }}
+                >
+                    <TextField
+                        label="Group Name:"
+                        type="text"
+                        value={groupName}
+                        onChange={(e) => setGroupName(e.target.value)}
+                        placeholder="Group name here..."
+                        required
+                        sx={{ mb: 1, width: '90%' }}
+                    />
+                    <TextField
+                        label="Group Description:"
+                        type="text"
+                        value={groupDescription}
+                        onChange={(e) => setGroupDescription(e.target.value)}
+                        placeholder="Group Description here..."
+                        required
+                        sx={{ mb: 1, width: '90%' }}
+                    />
+                    {/* <TextField
             label="Challenge Type:"
             type="text"
             value={challengeType}
@@ -126,7 +129,7 @@ const CreateGroupForm = () => {
             required
             sx={{ mb: 1 }}
           /> */}
-          {/* <input
+                    {/* <input
             label="Start Date"
             type="date"
             style={{ mb: 1 }}
@@ -154,22 +157,22 @@ const CreateGroupForm = () => {
                 : null
             }
           /> */}
-          <button
-            type="submit"
-            style={{
-              padding: "0.6rem 2.5rem",
-              backgroundColor: "#00647A",
-              color: "white",
-              fontWeight: "bold",
-              borderRadius: "0.7rem",
-            }}
-          >
-            Create Group
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                    <button
+                        type="submit"
+                        style={{
+                            padding: '0.6rem 2.5rem',
+                            backgroundColor: '#00647A',
+                            color: 'white',
+                            fontWeight: 'bold',
+                            borderRadius: '0.7rem',
+                        }}
+                    >
+                        Create Group
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default CreateGroupForm;
