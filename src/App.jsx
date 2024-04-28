a27b807fa810ebfe0c72a4842e68f7a74c2061fb
Sayyadsulthan <asulthan088@gmail.com>
2024-04-28 14:15:31 +0530

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// App main component in the website and has routing for website navigation
-// Imports
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/navbar/Navbar.jsx';
-import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 
 import { jwtDecode } from 'jwt-decode';
 import Home from './screens/home/Home.jsx';
@@ -16,19 +14,9 @@ import ShowMembers from './screens/privategroups/privategropmembernew.jsx';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, updateUser } from './slices/authSlice.js';
-// import PublicGroup from "./screens/publicgroups/PublicGroup.jsx";
 import PrivateGroup from './screens/privategroups/PrivateGroup.jsx';
 import ComingSoon from './components/comingsoon/ComingSoon.jsx';
 import Loading from './components/loading/Loading.jsx';
-// const ProtectedRoutes = ({ childrens, ...rest }) => {
-//     const auth = useSelector((state) => state.auth);
-//     // if the user authenticated go to that desired routes
-//     if (auth.isLoggedIn) {
-//         return <Outlet />;
-//     }
-
-//     return <Navigate to="/" />;
-// };
 
 const ProtectedRoutes = ({ children, ...rest }) => {
     const dispatch = useDispatch();
@@ -60,13 +48,9 @@ const ProtectedRoutes = ({ children, ...rest }) => {
 
     const auth = useSelector((state) => state.auth);
 
-    // Render children only if user is logged in
-    // return auth.isLoggedIn ? <Outlet /> : <Navigate to="/" />;
-    // Render children only if user is logged in
     if (auth.isLoggedIn) {
         return <Outlet />;
     } else if (loading) {
-        // You can render a loading indicator here if needed
         return <Loading />;
     } else {
         return <Navigate to="/" />;
@@ -75,9 +59,9 @@ const ProtectedRoutes = ({ children, ...rest }) => {
 
 const App = () => {
     const dispatch = useDispatch();
-    // const data = useSelector((state) => state.auth);
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         try {
             setLoading(true);
@@ -85,26 +69,22 @@ const App = () => {
             if (token) {
                 const userData = jwtDecode(token);
                 setUser(userData);
-                // const { name, email, pic, id } = userData;
                 dispatch(updateUser({ ...userData }));
                 dispatch(login());
-                setLoading(false);
             }
         } catch (err) {
             console.error(err);
+        } finally {
             setLoading(false);
         }
     }, []);
-
-    useEffect(() => {
-        setLoading(false);
-    }, [user, loading]);
 
     const handleLogout = () => {
         setUser('');
         setLoading(false);
         dispatch(logout());
     };
+
     if (loading) {
         return <Loading />;
     }
@@ -112,35 +92,20 @@ const App = () => {
     return (
         <div>
             <Router>
-                <Navbar /* isLoggedIn={user ? true : false}*/ logoutUser={handleLogout} />
+                <Navbar logoutUser={handleLogout} />
                 <ToastContainer position="bottom-center" />
-                <video autoPlay muted loop id="bg-video">
-                    <source
-                        // src="https://videos.pexels.com/video-files/4325592/4325592-uhd_4096_2160_25fps.mp4"
-                        // src="https://videos.pexels.com/video-files/5319099/5319099-uhd_3840_2160_25fps.mp4"
-                        src="https://videos.pexels.com/video-files/4761424/4761424-uhd_4096_2160_25fps.mp4"
-                        type="video/mp4"
-                    />
-                    Your browser does not support the video tag.
-                </video>
-
                 <div className="website-background">
                     <Routes>
-                        {/* {user ? (
-                            <> */}
-                        {/* <Route path="/profile" element={<Profile />} /> */}
                         <Route path="/editprofile" element={<EditProfile />} />
                         <Route path="/publicgroups" element={<ComingSoon />} />
                         <Route path="/profile" element={<ProtectedRoutes />}>
-                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/" element={<Profile />} />
                         </Route>
-                        <Route path="/creategroup" element={<CreateGroup />} />{' '}
-                        <Route path="/groups/:id/createchallenge" element={<CreateChallenge />} />{' '}
+                        <Route path="/creategroup" element={<CreateGroup />} />
+                        <Route path="/groups/:id/createchallenge" element={<CreateChallenge />} />
                         <Route path="/privategroup/:id/members" element={<ShowMembers />} />
-                        {/* <Route path="/joinpublicgroup/:id" element={<PublicGroup />} /> */}
                         <Route path="/privategroup/:id" element={<PrivateGroup />} />
                         <Route path="/" element={<Home />} />
-                        {/* <Route path="/resetpassword" element={<ResetPassword />} /> */}
                         <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
                 </div>
